1cb07d55c27eb3f8835f638febe2b2985bd6b05d
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-22 19:26:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,10 @@ def sort_array_asc(array)
     array.sort
 end
 
-def sort_array_desc
+def sort_array_desc(array)
     array.sort{ |a,b| b <=> a }
+end
+
+def sort_array_char_count
+    array.sort { |a,b| a.length <=> b.length }
 end
